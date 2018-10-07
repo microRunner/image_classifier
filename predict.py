c48c3280b28ab2af5e36deeba005ff4f1f85e9f2
Rahil Gupta <p10rahilg@iimahd.ernet.in>
2018-10-07 16:54:36 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,29 +6,30 @@ import json
 parser = argparse.ArgumentParser(description='predict.py')
 parser.add_argument('--image_path', type = str, default = 'flowers/test/101/image_07983.jpg',
                     help = 'path of the image to be predicted')
+parser.add_argument('--checkpoint',  type = str, default = 'saved_model.pth', help = 'Directory to load the saved model')
 parser.add_argument('--top_k',  type = int, default = 5, help = 'No of classes and probabilites to be returned')
-parser.add_argument('--save_dir',  type = str, default = 'saved_model.pth', help = 'Directory to save the model')
 parser.add_argument('--category_name', default = 'cat_to_name.json', help = 'Determine GPU vs CPU for the neural network')
 parser.add_argument('--gpu', type = str, default = 'gpu', help = 'Determine GPU vs CPU for the neural network')
 
 args = parser.parse_args()
 image_path = args.image_path
-save_dir = args.save_dir
+checkpoint = args.checkpoint
 gpu = args.gpu
 top_k = args.top_k
+category_name = args.category_name
 
 if gpu == 'gpu':
     is_gpu = True
 else:
     is_gpu = False
 
-model = load_model(save_dir, is_gpu)
+model = load_model(checkpoint, is_gpu)
 
 # to check if the model has been correctly built, and has significant accuracy
 # image_datasets, dataloaders = load_data('flowers')
 # check_accuracy(model, dataloaders['test'], is_gpu)
 
-with open('cat_to_name.json', 'r') as f:
+with open(category_name, 'r') as f:
     cat_to_name = json.load(f)
     
 predict_names(model, image_path, cat_to_name, top_k)
